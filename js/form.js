4c3a5c1ba5cf843fe87d7d7c862038ffe8cf5bb1
Barahlish <Barahlish@gmail.com>
2017-08-12 19:01:28 +0300

    v 2.0

---
+++ js/form.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#form').submit(function(){
+        $.ajax({
+            type: "POST",
+            url: "mail/mail.php",
+            data: $(this).serialize(),
+            success: function(data){
+                alert(data);
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                alert(jqXHR.status + errorThrown);
+            }
+        })
+    });
+});
---
+++ mail/mail.php
@@ -0,0 +1,22 @@
+<?php
+    $subject = trim($_POST["subject"]);
+    $email = trim($_POST["email"]);
+    $description = trim($_POST["description"]);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "E-mail false";
+    }
+
+    $admin = "Barahliush@gmail.com";
+    $email_body = $description;
+    $email_body .= "\r\nFrom: ";
+    $email_body .= $email;
+
+    $headers = "From: noreply@gmail.com\r\n";
+
+    if (mail($admin, subject, $email_body, $headers)) {
+        echo true;
+    } else {
+        echo false;
+    }
+?>
